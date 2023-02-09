9a52438a401bf6998086b0c0e34bd06711904d54
user <ayanakim258@gmail.com>
2023-02-09 22:19:06 +0600

    Initial commit

---
+++ app/src/test/java/com/example/alculator_/MathTest.kt
@@ -0,0 +1,40 @@
+package com.example.alculator_
+
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class MathTest {
+    private var math:Math? = null
+    @Before
+    fun before(){
+        math = Math()
+    }
+    @Test
+    fun add(){
+        math?.add("2","2")
+        math?.add("1","1")
+    }
+
+    @Test(expected = ArithmeticException::class)
+    fun divide(){
+        math?.divide("0","8")
+    }
+
+    @Test
+    fun minus(){
+        math?.minus("0","8")
+    }
+
+    @Test
+    fun multiply(){
+        math?.multiply("2","8")
+    }
+
+    @After
+    fun after(){
+        math = null
+    }
+
+}
